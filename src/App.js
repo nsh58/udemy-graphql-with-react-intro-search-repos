ca063b84823ac883d69e89f4bab6bb69b6fbd1e2
georgia <georgia6954@gmail.com>
2020-06-21 17:50:04 +0900

    setup dotenv

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
+const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
+
+console.log(GITHUB_TOKEN);
+
 function App() {
   return (
     <div className="App">
       <p>Hi</p>
+      
     </div>
   );
 }
