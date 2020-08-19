bf31631b6d3f6fd0d4d7cd291316f9556114db99
georgia <georgia6954@gmail.com>
2020-08-19 14:45:23 +0900

    sync-star

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ import client from './client.js'
 import { ADD_STAR, REMOVE_STAR, SEARCH_REPOSITORIES } from './graphql'
 
 const StarButton = props => {
-  console.log(props)
-  const node = props.node;
-  // props.viewerHasStarred
+  const {node, query, first, last, after, before } = props;
   const totalCount = node.stargazers.totalCount
   const viewerHasStarred = node.viewerHasStarred
   const starCount = totalCount === 1 ? "1 star" : `${totalCount} stars`
@@ -25,7 +23,16 @@ const StarButton = props => {
   }
 
   return (
-    <Mutation mutation={viewerHasStarred ? REMOVE_STAR : ADD_STAR}>
+    <Mutation
+      mutation={viewerHasStarred ? REMOVE_STAR : ADD_STAR}
+      refetchQueries={ mutationResult => {
+        return [
+          {
+            query: SEARCH_REPOSITORIES,
+            variables: {query, first, last, before, after}
+          }
+        ]
+      }}>
       {
         addOrRemoveStar => <StarStatus addOrRemoveStar={addOrRemoveStar}/>
       }
@@ -107,7 +114,7 @@ class App extends Component {
                         return (
                           <li key={node.id}>
                             <a href={node.url} target="_blank" rel="noopener noreferrer">{node.name}</a>
-                            <StarButton node={node}></StarButton>
+                            <StarButton node={node} {...{query, first, last, after, before}}></StarButton>
                           </li>
                         )
                       })
